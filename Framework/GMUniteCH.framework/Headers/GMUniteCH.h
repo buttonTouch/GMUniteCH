efcaac761051f3e3d744988c3bdae93afd6728b1
WY <18824676521@163.com>
2021-07-06 15:49:57 +0800

    1.0.4

--- Framework/GMUniteCH.framework/Headers/GMUniteCH.h
+++ Framework/GMUniteCH.framework/Headers/GMUniteCH.h
@@ -16,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char GMUniteCHVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <GMUniteCH/PublicHeader.h>
 #import <GMUniteCH/GMCoreManager.h>
 #import <GMUniteCH/GMTools.h>
+#import <GMUniteCH/GMAdManager.h>
 #import <GMUniteCH/GMTjManager.h>
 
