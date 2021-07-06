f7e6bbe41b5169f88ce674eabfc426929615dc0e
WY <18824676521@163.com>
2021-07-06 16:16:54 +0800

    1.0.5

---
+++ Framework/GMUniteCH.framework/PrivateHeaders/GMWebViewController.h
@@ -0,0 +1,16 @@
+//
+//  GMWebViewController.h
+//  GMUniteCH
+//
+//  Created by Mgs on 2021/5/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GMWebViewController : UIViewController
+@property(nonatomic,strong)NSString *urlString;
+@end
+
+NS_ASSUME_NONNULL_END
